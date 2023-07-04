afa5fb52849f28e180560f99db09eda5bb2b5b2f
_NIROB/'HASAN_!! <134780028+Jok4rsoj1b4@users.noreply.github.com>
2023-07-04 21:55:20 +0600

    Delete FMZ-1.py

--- FMZ-1.py
+++
@@ -1,8 +0,0 @@
-import os,platform
-os.system('FMZ.py')
- 
-trt=platform.architecture()[0]
-if trt=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif trt=="64bit":
-    __import__("FMZ-1.py")
